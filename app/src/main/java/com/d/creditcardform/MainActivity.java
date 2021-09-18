290192a38341fad427b7235a430096f070302b59
Ashish-BITS <you@example.com>
2021-09-18 19:10:35 +0400

    Initial commit

---
+++ app/src/main/java/com/d/creditcardform/MainActivity.java
@@ -0,0 +1,69 @@
+package com.d.creditcardform;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.braintreepayments.cardform.view.CardForm;
+
+public class MainActivity extends AppCompatActivity {
+
+    CardForm cardForm;
+    Button buy;
+    AlertDialog.Builder alertBuilder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cardForm = findViewById(R.id.card_form);
+        buy = findViewById(R.id.btnBuy);
+        cardForm.cardRequired(true)
+                .expirationRequired(true)
+                .cvvRequired(true)
+                .postalCodeRequired(true)
+                .mobileNumberRequired(true)
+                .mobileNumberExplanation("SMS is required on this number")
+                .setup(MainActivity.this);
+        cardForm.getCvvEditText().setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_VARIATION_PASSWORD);
+        buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (cardForm.isValid()) {
+                    alertBuilder = new AlertDialog.Builder(MainActivity.this);
+                    alertBuilder.setTitle("Confirm before Finish");
+                    alertBuilder.setMessage("Card number: " + cardForm.getCardNumber() + "\n" +
+                            "Card expiry date: " + cardForm.getExpirationDateEditText().getText().toString() + "\n" +
+                            "Card CVV: " + cardForm.getCvv() + "\n" +
+                            "Postal code: " + cardForm.getPostalCode() + "\n" +
+                            "Phone number: " + cardForm.getMobileNumber());
+                    alertBuilder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                            Toast.makeText(MainActivity.this, "Thank you for registering", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    });
+                    AlertDialog alertDialog = alertBuilder.create();
+                    alertDialog.show();
+
+                } else {
+                    Toast.makeText(MainActivity.this, "Please complete the form", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
